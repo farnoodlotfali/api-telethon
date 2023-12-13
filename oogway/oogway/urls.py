1d723591f0f56a3ea8b56942ae3ea3b8b21b4035
faridlotfali <farid9lotfali@gmail.com>
2023-12-13 11:10:41 +0400

    add panel

--- oogway/oogway/urls.py
+++ oogway/oogway/urls.py
@@ -3,5 +3,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('analyze/', include('PostAnalyzer.urls')),
+    path('panel/', include('Panel.urls')),
     path('admin/', admin.site.urls),
 ]
