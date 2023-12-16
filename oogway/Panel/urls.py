f41e553e4e9ff104a7b301148f3f2cf1e506aa31
faridlotfali <farid9lotfali@gmail.com>
2023-12-16 22:35:47 +0400

    change dashboard design and add login

--- oogway/Panel/urls.py
+++ oogway/Panel/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
+     path('login/', views.CustomLoginView.as_view(template_name='login.html'), name='login'),
+     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
      path('', views.home, name='home')
 ]
--- oogway/Panel/views.py
+++ oogway/Panel/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LoginView
 
-# Create your views here.
-
+@login_required(login_url='login')
 def home(request):
     return render(request, 'Home/home.html')
+class CustomLoginView(LoginView):
+    def get_success_url(self):
+        user = self.request.user
+        if user.is_superuser:
+            return '/panel'
+
